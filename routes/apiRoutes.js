9e09b4dafdd8ee26a88fe77f18651ad9269de429
Joanna Preston <joannappreston@gmail.com>
2020-05-13 11:28:07 -0700

    styling homepage

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,7 +7,7 @@ const jsonData = require("../db/db");
 
 module.exports = function(app) {
 
-    // GET - view api data
+    // GET - get api data
     app.get("/api/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../db/db.json"));
     });
